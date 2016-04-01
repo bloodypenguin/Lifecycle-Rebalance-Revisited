0b2a234bc93b13bbf02ec382da438a8790c46223
WhitefangGreytail <whitefang_greytail@yahoo.com.au>
2016-04-02 00:05:48 +0800

    Rename mod to be more correct

--- WG_CitizenEdit/Code/info.cs
+++ WG_CitizenEdit/Code/info.cs
@@ -7,7 +7,7 @@ namespace WG_CitizenEdit
     {
         public string Name
         {
-            get { return "WG Citizen Life Rebalance v1.2"; }
+            get { return "WG Citizen Lifecycle Rebalance v1.2"; }
         }
         public string Description
         {
