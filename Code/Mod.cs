f1081d7e0c2a5cbc76a5f0797945ad6372d96bac
algernon-A <aeneas@aloku.net>
2021-12-17 11:07:43 +1100

    Version bump to 1.6 final.

--- Code/Mod.cs
+++ Code/Mod.cs
@@ -18,8 +18,8 @@ namespace LifecycleRebalance
         // Internal and private name and version components.
         internal static string ModName => "Lifecycle Rebalance Revisited";
         internal static string Version => BaseVersion + " " + Beta;
-        internal static string Beta => "BETA 3";
-        internal static int BetaVersion => 3;
+        internal static string Beta => "";
+        internal static int BetaVersion => 0;
         private static string BaseVersion => "1.6";
 
 
